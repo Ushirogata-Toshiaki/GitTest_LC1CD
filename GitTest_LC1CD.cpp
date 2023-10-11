86b7d12815092c1b5acbd40ffb16ab32e70c6594
Ushirogata-Toshiaki <114214733+Ushirogata-Toshiaki@users.noreply.github.com>
2023-10-11 14:34:17 +0900

    日付の出力

--- GitTest_LC1CD.cpp
+++ GitTest_LC1CD.cpp
@@ -8,6 +8,8 @@ int main()
     std::cout << "Hello Github\n";
 
     std::cout << "LC1C_00_ｳｼﾛｶﾞﾀ_ﾄｼｱｷ\n";
+
+    std::cout << "2023/10/11\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
